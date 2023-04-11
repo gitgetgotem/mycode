82710ffaf1bbb471d4a2d91de1e6f189bf3db64a
gitgetgotem <matthewamalanga@gmail.com>
2023-04-11 20:21:54 +0000

    Avatar Determiner

---
+++ avatar_picker.py
@@ -0,0 +1,52 @@
+#!/bin/usr/env python3
+
+def yes_no_response(response):
+    valid_response = ["yes", "yeah", "y", "ya", "ye"]
+    invalid_response = ["no", "n", "nope", "nah"]
+    while True:
+        if response.lower() in valid_response:
+            return True
+        elif response.lower() in invalid_response:
+            return False
+        else:
+            print("Invalid answer. A true Bender would know to keep their answers simple! ")
+
+def question1():
+    response = input("While out in the world, do you take the time to appreciate the little things? ")
+    return yes_no_response(response)
+
+def question2():
+    response = input("Do you believe in the concepts of Yin and Yang, Good vs. Evil? ")
+    return yes_no_response(response)
+
+def question3():
+    response = input("Is it possible to attain true enlightenment? ")
+    return yes_no_response(response)
+
+def bender_score():
+    score = 0
+    if question1():
+        score += 1
+    if question2():
+        score += 1
+    if question3():
+        score +=1
+
+    if score == 3:
+        return "Airbender"
+    if score == 2:
+        return "Waterbender"
+    if score == 1:
+        return "Firebender"
+    else:
+        return "Earthbender"
+
+def main():
+    print("""So you think you have what it takes to bend the elements?
+Well let's find out then, shall we? """)
+    bender_type = bender_score()
+    print(f"You are a {bender_type}!")
+        
+
+if __name__ == "__main__":
+    main()
