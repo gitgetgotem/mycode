9280c9640821db3f5293f910aa1000aac71dcdd1
gitgetgotem <matthewamalanga@gmail.com>
2023-04-11 22:32:00 +0000

    Update

--- avatar_picker.py
+++ avatar_picker.py
@@ -1,17 +1,19 @@
 #!/bin/usr/env python3
+"""The official Avatar Picker is here! Take the test to determine your bending abilities!"""
 
-def yes_no_response(response):
-    valid_response = ["yes", "yeah", "y", "ya", "ye"]
+def yes_no_response(response):     # This function takes the users response, checks it against the given lists,
+    valid_response = ["yes", "yeah", "y", "ya", "ye"]  # and returns a Boolean Value
     invalid_response = ["no", "n", "nope", "nah"]
     while True:
-        if response.lower() in valid_response:
+        if response.lower() in valid_response:    # using .lower() to make things easier
             return True
         elif response.lower() in invalid_response:
             return False
         else:
             print("Invalid answer. A true Bender would know to keep their answers simple! ")
-
-def question1():
+            response = input("Please respond with either Yes or No: ") # In the case of an invalid response, prompts
+                                                                       # the user for a new response
+def question1(): # This is where I am defining the questions and passing the yes_no_response function into them
     response = input("While out in the world, do you take the time to appreciate the little things? ")
     return yes_no_response(response)
 
@@ -23,7 +25,7 @@ def question3():
     response = input("Is it possible to attain true enlightenment? ")
     return yes_no_response(response)
 
-def bender_score():
+def bender_score(): # I am using a counter based of the Boolean values of the responses to the queastions
     score = 0
     if question1():
         score += 1
@@ -31,20 +33,20 @@ def bender_score():
         score += 1
     if question3():
         score +=1
-    
+    # creating a dictionary that pairs the "score" to the "bender_type" 
     bender_types = {
             3 : "Airbender",
             2 : "Waterbender",
             1 : "Firebender",
             0 : "Earthbender"
         }
-    return bender_types.get(score, "Invalid score")
-
+    return bender_types.get(score, "Invalid score") # This uses .get() to pull the associated value for the "bender_types" key
+# our main function
 def main():
     print("""So you think you have what it takes to bend the elements?
 Well let's find out then, shall we? """)
     bender_type = bender_score()
-    print(f"You are a {bender_type}!")
+    print(f"You are a(n) {bender_type}!")
         
 
 if __name__ == "__main__":
