cd8589622d7022c90b4a23a00dfd8370c11b92a0
gitgetgotem <matthewamalanga@gmail.com>
2023-04-12 18:23:09 +0000

    Updating code

--- avatar_picker.py
+++ avatar_picker.py
@@ -4,16 +4,23 @@
 def yes_no_response(response):     # This function takes the users response, checks it against the given lists,
     valid_response = ["yes", "yeah", "y", "ya", "ye"]  # and returns a Boolean Value
     invalid_response = ["no", "n", "nope", "nah"]
+    help_menu = ["h", "help", "?"]
     while True:
         if response.lower() in valid_response:    # using .lower() to make things easier
             return True
         elif response.lower() in invalid_response:
             return False
+        elif response.lower() in help_menu:
+            print("It doesn't get any simpler than yes or no responses!")
+            response = input("Answer the question with either yes or no: ")
         else:
             print("Invalid answer. A true Bender would know to keep their answers simple! ")
             response = input("Please respond with either Yes or No: ") # In the case of an invalid response, prompts
-                                                                       # the user for a new response
-def question1(): # This is where I am defining the questions and passing the yes_no_response function into them
+# the user for a new response
+
+
+# This is where I am defining the questions and passing them through my yes_no_response function
+def question1():
     response = input("While out in the world, do you take the time to appreciate the little things? ")
     return yes_no_response(response)
 
@@ -42,6 +49,8 @@ def bender_score(): # I am using a counter based of the Boolean values of the re
         }
     return bender_types.get(score, "Invalid score") # This uses .get() to pull the associated value for the "bender_types" key
 # our main function
+
+
 def main():
     print("""So you think you have what it takes to bend the elements?
 Well let's find out then, shall we? """)
