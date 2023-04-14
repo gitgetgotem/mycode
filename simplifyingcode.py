754805cbcf9d707fd7168970e430d2abd3ce4900
gitgetgotem <matthewamalanga@gmail.com>
2023-04-14 15:59:47 +0000

    update

--- simplifyingcode.py
+++ simplifyingcode.py
@@ -1,10 +1,11 @@
 #!/bin/usr/env python3
 """Figuring out your Zodiac Sign!"""
-usr_name = input("What is your name? ")
-usr_name = usr_name.title()
+usr_name = input("What is your name? ").title()
 year = int(input("What year were you born? --> "))
+zodiac_year = year % 12
 
-sign = {4 : ["Rabbit", "vigilant, witty, quick-minded, and ingenious."],
+sign = {
+        4 : ["Rabbit", "vigilant, witty, quick-minded, and ingenious."],
         5 : ["Rat", "artistic, sociable, industrious, charming, and intelligent."],
         6 : ["Tiger", "courageous, enthusiastic, confident, charismatic, and a leader."],
         7 : ["Ox", "strong, thorough, determined, loyal, and reliable."],
@@ -18,5 +19,4 @@ sign = {4 : ["Rabbit", "vigilant, witty, quick-minded, and ingenious."],
         3 : ["Pig", "a symbol of wealth, honesty, and practicality."]
         }
 
-zodiac_year = year % 12
 print(f"{usr_name} your zodiac sign is {sign[zodiac_year][0]}, you are {sign[zodiac_year][1]}")
