6feecb6d876f5adbbf0a18c86d4837b234d941a7
gitgetgotem <matthewamalanga@gmail.com>
2023-04-14 18:36:37 +0000

    Working with Flask

--- pokemon/pokemon_api.py
+++ pokemon/pokemon_api.py
@@ -2,6 +2,8 @@
 
 import requests
 
+from pprint import pprint
+
 def main():
     pokenum= input("Pick a number between 1 and 151!\n>")
     pokeapi= requests.get("https://pokeapi.co/api/v2/pokemon/" + pokenum).json()
@@ -9,6 +11,10 @@ def main():
     print(pokeapi['sprites']['front_default'])
     for move in pokeapi['moves']:
         print(move['move']['name'])
+    pprint(pokeapi['game_indices'])
+    count = 0
+    game_count = len(pokeapi['game_indices'])
+    print(game_count)
 
 main()
 
