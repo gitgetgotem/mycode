124484ba3ebb560bf4b5a61f417026b89f6f7344
gitgetgotem <matthewamalanga@gmail.com>
2023-04-19 22:22:00 +0000

    updates

--- alta3_code/99bottles.py
+++ alta3_code/99bottles.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 def main():
-    bottles = int(input("How many bottles should start on the wall? ")) + 1
-    if bottles > 101:
-        print("You should call (800) 839-1686 and get some help")           
-    else:
-        for x in range(bottles):
-            bottles -= 1
-            print(f"""{bottles} bottles of beer on the wall!
+    while True:
+        try:
+            bottles = int(input("How many bottles should start on the wall? ")) + 1
+            if bottles > 101:
+                print("You should call (800) 839-1686 and get some help")           
+            else:
+                for x in range(bottles):
+                    bottles -= 1
+                    print(f"""{bottles} bottles of beer on the wall!
 {bottles} bottles of beer! Take one down, pass it around!""")
+            break
+        except ValueError:
+            print("Invalid input. Enter a number: ")
+  
 
 if __name__ == "__main__":
     main()
--- my_projects/final_project/scratchfile01.py
+++ my_projects/final_project/scratchfile01.py
@@ -1,6 +1,6 @@
+#give list of winners, have users input which they would like to know more about
 from pprint import pprint
 import requests
-import json
 import http
 
 # https://www.nobelprize.org/about/developer-zone-2/
@@ -10,10 +10,10 @@ import http
 #resp = requests.get(f"{API}")
 
 
-url = "http://api.nobelprize.org/v1/country.json"
+url = "https://api.nobelprize.org/2.1/nobelPrizes"
 
 headers = {"accept": "application/json"}
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers).json()
 
-pprint(response.text)
+pprint(response["nobelPrizes"][0])
